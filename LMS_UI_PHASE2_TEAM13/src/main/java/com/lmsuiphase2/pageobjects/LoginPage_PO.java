4a05e0f55994f1f35ab99ae0341cf3fc9780f9d7
SowmyaSenthilkumar10 <User@DESKTOP-GFCOMV2>
2023-09-14 11:26:16 -0400

    Merging Sowmya Branch -Login, Batch

--- LMS_UI_PHASE2_TEAM13/src/main/java/com/lmsuiphase2/pageobjects/LoginPage_PO.java
+++ LMS_UI_PHASE2_TEAM13/src/main/java/com/lmsuiphase2/pageobjects/LoginPage_PO.java
@@ -1,5 +1,170 @@
 package com.lmsuiphase2.pageobjects;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import com.lmsuiphase2.pageobjects.Common_PO;
+import com.lmsuiphase2.pageobjects.DashboardPage_PO;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.lmsuiphase2.utilities.CommonUtils;
+import com.lmsuiphase2.utilities.ExcelUtils;
+
 public class LoginPage_PO {
+
+	WebDriver driver;
+	public Actions action;
+	Common_PO CommonPO;
+
+	public @FindBy(id = "Logo") WebElement Logo;
+	public @FindBy(id = "username") WebElement User;
+	public @FindBy(id = "password") WebElement Password;
+	public @FindBy(xpath = "//input[text()='Login']") WebElement LoginBtn;
+	public @FindBy(id = "forget username/password") WebElement Forgetusername_password_Link;
+	public @FindBy(id = "Forgetusername_password_Page") WebElement Forgetusername_password_Page;
+	public @FindBy(id = "reset password") WebElement Resetpassword_link;
+	public @FindBy(id = "reset password page") WebElement Resetpassword_Page;
+	public @FindBy(id = "forgetpassword_emailid") WebElement Emailid;
+	public @FindBy(id = "sendlink") WebElement Send_LinkBtn;
+	public @FindBy(id = "new password") WebElement Newpassword;
+	public @FindBy(id = "retype password") WebElement Retype_Password;
+	public @FindBy(id = "Submit") WebElement SubmitBtn;
+	public @FindBy(id = "LMS Application") WebElement LoginTitle;
+	public @FindBy(xpath = "//div[text()='errormessage']") WebElement errMsg;
+
+	String errorMessage;
+	String applicationUrl = CommonUtils.applicationUrl;
+	String loginPageURL = CommonUtils.loginUrl;
+	String excelURL = CommonUtils.excelFilePath;
+	String invalidapp_Url = "http://heruapp.com/";
+	String dashbaordUrl = CommonUtils.dashbaordUrl;
+
+	public LoginPage_PO(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		action = new Actions(this.driver);
+
+		CommonPO = new Common_PO();
+
+	}
+
+	public void homepageWithvalidURL() {
+		driver.get(applicationUrl);
+	}
+
+	public void homepageWithInvalidURL() {
+		driver.get(invalidapp_Url);
+	}
+
+	public void loginHeader() {
+		LoginTitle.getText();
+	}
+
+	public void dashboardPage() {
+		driver.get(dashbaordUrl);
+	}
+
+	public boolean getErrorMessage(String errormsg) {
+		return errMsg.getText().equals(errormsg);
+	}
+
+	// LoginPage
+	public void Login() {
+		driver.get(loginPageURL);
+	}
+
+	public void enterUser(String user) {
+		User.sendKeys(user);
+		LoginBtn.click();
+
+	}
+
+	public void enterPassword(String password) {
+		Password.sendKeys(password);
+		LoginBtn.click();
+	}
+
+	public void adminLoginWithValidCredentials(String user, String password) {
+		User.sendKeys(user);
+		Password.sendKeys(password);
+	}
+
+	public void adminLoginWithInvalidCredentials(String user, String password) {
+
+		User.sendKeys(user);
+		Password.sendKeys(password);
+	}
+
+	public void LoginWithValidCredentialsInUsername(String user) {
+
+		User.sendKeys(user);
+
+	}
+
+	public void LoginWithEmptyValuesInBothField(String user, String password) {
+
+		User.sendKeys(user);
+		Password.sendKeys(password);
+
+	}
+
+	public void LoginButtonActionThroughKeyboard(String user, String password) {
+
+		User.sendKeys(user);
+		Password.sendKeys(password);
+		LoginBtn.sendKeys(Keys.ENTER);
+	}
+
+	public void LoginButtonActionThroughMouse(String user, String password) {
+
+		User.sendKeys(user);
+		Password.sendKeys(password);
+		LoginBtn.click();
+	}
+
+	// ForgotUsername Or Password
+
+	public void emailSentForForgotPassword(String email) {
+		Emailid.sendKeys(email);
+		Send_LinkBtn.click();
+
+	}
+
+	public void sentEmailForForgotPasswordWithInvalidEmailid(String email) {
+		Emailid.sendKeys(email);
+		Send_LinkBtn.click();
+
+	}
+
+	// Reset Password
+
+	public void resetPasswordWithValidDetails(String newpassword, String retypepassword) {
+		Newpassword.sendKeys(newpassword);
+		Retype_Password.sendKeys(retypepassword);
+		SubmitBtn.click();
+
+	}
+
+	public void resetPasswordWithInvalidDetails(String newpassword, String retypepassword) {
+		Newpassword.sendKeys(newpassword);
+		Retype_Password.sendKeys(retypepassword);
+		SubmitBtn.click();
+	}
+
+	public void resetPasswordWithEmptyDetails(String newpassword, String retypepassword) {
+		Newpassword.sendKeys(newpassword);
+		Retype_Password.sendKeys(retypepassword);
+		SubmitBtn.click();
+	}
+
+	public void resetPasswordMismatchValues(String newpassword, String retypepassword) {
+
+		Newpassword.sendKeys(newpassword);
+		Retype_Password.sendKeys(retypepassword);
+		SubmitBtn.click();
+
+	}
 
 }
